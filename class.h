58c78b96100b189756e05615afd3b0fd8a1f0d7c
Alforreal <59706385+Alforreal@users.noreply.github.com>
2022-02-23 13:19:54 +0200

    Add files via upload

---
+++ class.h
@@ -0,0 +1,14 @@
+#ifndef KRAMMER_H
+#define KRAMMER_H
+class Krammer
+{
+    public:
+        int inp2[2][3];
+        int inp3[3][4]; 
+        int delta3[4];
+        int delta2[3];
+        int X,Y,Z;
+};
+
+
+#endif
